1bd640987486fc54505aa1b01f5a4c68fb79f869
chenwh <chenwh@eastcom-sw.com>
2021-01-19 17:01:21 +0800

    add *

--- string/lt4_d++MedianOfTwoSortedArray.cpp
+++ string/lt4_d++MedianOfTwoSortedArray.cpp
@@ -5,23 +5,53 @@
 using namespace std;
 
 /*
- * 问题:求最长不重复字符串
- * 解法:滑动窗口
+ * Median of Two Sorted Arrays
+ * 解法:二分法
  *
  * */
 //
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int sum = nums1.size() + nums2.size();
+        int half = sum / 2;
+        int left = 0;
+        int right = nums1.size();
+        int lMax = 0;
+        int rMin = 0;
 
+        while (left <= right) {
+            int i = (left + right) >> 1;
+            int j = half - i;
+            //i 整体太大
+
+            int li = i == 0 ? INT_MIN : nums1[i - 1];
+            int ri = i == nums1.size() ? INT_MAX : nums1[i];
+            int lj = j == 0 ? INT_MIN : nums2[j - 1];
+            int rj = j == nums2.size() ? INT_MAX : nums2[j];
+
+            if (li < rj){
+                lMax = max(li, lj);
+                rMin = min(ri, rj);
+                left = i + 1;
+            }else{
+                right = i - 1;
+            }
+
+        }
+        return sum % 2 == 0 ? (lMax + rMin) / 2.0 : rMin;
     }
 };
 
 int main(){
-//    Solution a = Solution();
-//    cout << a.lengthOfLongestSubstring("bbbbbcbb") ;
-//    unordered_set<int> set = {1, 2, 3, 4, 5};
-//    cout << max(1,(int)set.size());
+    Solution a = Solution();
+    vector<int> v1({1, 2});
+    vector<int> v2({3, 4});
+
+    cout << a.findMedianSortedArrays(v1,v2) ;
 
 }
 
