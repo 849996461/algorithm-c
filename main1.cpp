bc1700c4f8d493e69c755f9f0f8803486427fd4d
chenwh <chenwh@eastcom-sw.com>
2021-01-19 09:33:32 +0800

    add *

--- main1.cpp
+++ main1.cpp
@@ -2,31 +2,65 @@
 #include "unordered_set"
 #include "set"
 #include "math.h"
+#include "vector"
+#include "list"
+
 using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        unordered_set<char> rem ;
-        int ml = 0;
-        int rk = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            while (rk < s.size() && !rem.count(s[rk])) {
-                rem.insert(s[rk]);
-                rk++;
-            }
-            ml = max(ml,(int)rem.size());
-            rem.erase(s[i]);
-
+    vector<vector<int>> zigzagLevelOrder(TreeNode *root) {
+        vector<vector<int>> res;
+        if (!root){
+            return res;
         }
-        return ml;
+        list<TreeNode *> list = {root};
+        bool order = true;
+        while (!list.empty()) {
+            int size = list.size();
+            vector<int> row;
+            for (int i = 0; i < size; ++i) {
+                TreeNode node = *list.back();
+                list.pop_back();
 
+                if (order) {
+                    if (node.left) {
+                        list.push_back(node.left);
+                    }
+                    if (node.right) {
+                        list.push_back(node.right);
+                    }
+                }
+                else{
+                    if (node.right) {
+                        list.push_back(node.right);
+                    }
+                    if (node.left) {
+                        list.push_back(node.left);
+                    }
+                }
+                row.push_back(node.val);
+            }
+            order = !order;
+            res.push_back(row);
+        }
+        return res;
     }
 
 };
 
-int main(){
+int main() {
     Solution a = Solution();
-    cout << a.lengthOfLongestSubstring("bbbbbcbb") ;
+//    cout << a.zigzagLevelOrder();
 //    unordered_set<int> set = {1, 2, 3, 4, 5};
 //    cout << max(1,(int)set.size());
 
---
+++ string/lt4_d++MedianOfTwoSortedArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "unordered_set"
+#include "set"
+#include "math.h"
+using namespace std;
+
+/*
+ * 问题:求最长不重复字符串
+ * 解法:滑动窗口
+ *
+ * */
+//
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+    }
+};
+
+int main(){
+//    Solution a = Solution();
+//    cout << a.lengthOfLongestSubstring("bbbbbcbb") ;
+//    unordered_set<int> set = {1, 2, 3, 4, 5};
+//    cout << max(1,(int)set.size());
+
+}
+
