b94b197b0375dd75df3f93d0d8874063dd5bdb2c
chenwh <chenwh@eastcom-sw.com>
2021-01-23 14:40:36 +0800

    add *

--- main1.cpp
+++ main1.cpp
@@ -12,20 +12,33 @@ using namespace std;
 
 class Solution {
 public:
-    int maximumProduct(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int l = nums.size();
-        int max2 = nums[l-1] * nums[l-2] * nums[l-3];
-        int max4 = nums[l-1] * nums[1] * nums[0];
-        return max({ max2, max4});
+    vector<int> addToArrayForm(vector<int>& A, int K) {
+        int sum ;
+        vector<int> res;
+        for (int i = A.size()-1; i >= 0; --i) {
+            sum = A[i] + K % 10;
+            K/=10;
+            if (sum >= 10) {
+                K+=1;
+                sum %= 10;
+            }
+            res.push_back(sum);
+        }
+        for (; K > 0; K /= 10) {
+            res.push_back(K % 10);
+        }
+        reverse(res.begin(), res.end());
+        return res;
 
     }
 };
 
 int main() {
     Solution a = Solution();
-//    cout << a.zigzagLevelOrder();
-//    unordered_set<int> set = {1, 2, 3, 4, 5};
-//    cout << max(1,(int)set.size());
+    vector<int> v({1,2,0,0});
+    auto res = a.addToArrayForm(v, 34);
+    for (int i = 0; i < res.size(); ++i) {
+        cout << res[i];
+    }
 
 }
---
+++ string/lt6_d+ZigZag.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <math.h>
+#include <map>
+#include <iomanip>
+#include<queue>
+#include<set>
+#include<numeric>
+#include<unordered_set>
+using namespace std;
+
+
+/*
+ *
+ * */
+class Solution {
+public:
+    string convert(string s, int numRows) {
+
+    }
+
+};
+
+
+int main(){
+
+    Solution a = Solution();
+    string s = "12344321";
+    cout << a.convert(s, 1);
+
+}
+
+//
